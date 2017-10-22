6646dfad62f03d15010cc797cfd9068ddfce78a7
Abhinav MIshra <abhinavmishra094@gmail.com>
2017-10-22 12:35:32 +0530

    initial commit

--- src/SimpleClassConstructor.kt
+++ src/SimpleClassConstructor.kt
@@ -1,23 +1,24 @@
-class Car1(Type: String, Model: Int, Price: Double, MilesDrive: Int, Owner1: String) {
+class Car() {
     var Owner1: String? = null
     var Price: Double? = null
     var MilesDrive: Int? = null
     var Type: String? = null
     var Model: Int? = null
-
-    init {
-//        println("Type: $Type")
-//        println("Model: $Model")
-//        println("Price: $Price")
-//        println("MilesDrive: $MilesDrive")
-//        println("Owner: $Owner1")
+    constructor(Type: String, Model: Int, Price: Double, MilesDrive: Int, Owner1: String) : this() {
+        println("Type: $Type")
+        println("Model: $Model")
+        println("Price: $Price")
+        println("MilesDrive: $MilesDrive")
+        println("Owner: $Owner1")
         this.Owner1 = Owner1
         this.Price = Price
         this.MilesDrive = MilesDrive
         this.Type = Type
         this.Model = Model
     }
-
+    constructor(Name:String):this(){
+        println("new  class")
+    }
     fun GetOwner(): String? {
         return this.Owner1;
 
@@ -29,10 +30,11 @@ class Car1(Type: String, Model: Int, Price: Double, MilesDrive: Int, Owner1: Str
 }
 
 fun main(args: Array<String>) {
-    var huCar = Car1("BMW", 2015, 10000.0, 105, "abhinav")
+    var huCar = Car("BMW", 2015, 10000.0, 105, "abhinav")
     println("price: " + huCar.GetOwner())
     println("price: " + huCar.GetPrice())
-    var jeCar = Car1("BMW", 2017, 10000.0, 15, "abc")
+    var jeCar = Car("BMW", 2017, 10000.0, 15, "abc")
     println("price: " + jeCar.GetOwner())
     println("price: " + jeCar.GetPrice())
+    Car("abc")
 }
