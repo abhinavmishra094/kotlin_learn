d611ac7854fecff5ca1ce86f72712a8ee8ba5122
abhinav <abhinavmishra094@gmail.com>
2017-10-24 17:03:41 +0530

    multithreading

---
+++ src/MyThread.kt
@@ -0,0 +1,36 @@
+fun main(args: Array<String>) {
+    var t1 = thread("thread1")
+    t1.start()
+    var t2 = thread("thread2")
+    t2.start()
+    var t3 = thread("thread3")
+    t3.start()
+    t3.join()
+    println("  thread is run")
+}
+
+
+class thread() : Thread() {
+    var ThreadName:String= ""
+    constructor(ThreadName:String):this(){
+     this.ThreadName = ThreadName
+        println(this.ThreadName+" thread started")
+    }
+
+    override fun run() {
+        super.run()
+        var count = 0
+        while (count < 10) {
+            println(this.ThreadName+" Count: $count")
+            count++
+
+
+            try {
+                Thread.sleep(1000)
+            } catch (ex: Exception) {
+                println(ex.message)
+            }
+        }
+    }
+
+}
